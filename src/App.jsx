262384455398eb13c088f437e98048ad20d8db4f
ibrahimrangkuti <rangkutyibrahim765@gmail.com>
2023-03-23 21:05:07 +0700

    add: border top on footer

--- src/App.jsx
+++ src/App.jsx
@@ -81,101 +81,104 @@ export default function App() {
   };
 
   return (
-    <div className="container mx-auto mt-7">
-      <h3 className="text-xl">Jadwal Shalat</h3>
-      <h2 className="text-4xl font-semibold">
-        {selectedCity ? selectedCity : ""}
-      </h2>
-      <form className="mt-5 lg:mt-8 flex flex-col lg:flex-row gap-3">
-        <div className="w-full">
-          <label htmlFor="state">Provinsi</label>
-          <select
-            id="state"
-            className="w-full h-10 px-3 py-2 mt-2 focus:ring-1 focus:ring-slate-400 rounded outline-none shadow text-sm font-semibold font-comforta"
-            onChange={(e) => setSelectedState(e.target.value)}
+    <>
+      <div className="container mx-auto mt-7">
+        <h3 className="text-xl">Jadwal Shalat</h3>
+        <h2 className="text-4xl font-semibold">
+          {selectedCity ? selectedCity : ""}
+        </h2>
+        <form className="mt-5 lg:mt-8 flex flex-col lg:flex-row gap-3">
+          <div className="w-full">
+            <label htmlFor="state">Provinsi</label>
+            <select
+              id="state"
+              className="w-full h-10 px-3 py-2 mt-2 focus:ring-1 focus:ring-slate-400 rounded outline-none shadow text-sm font-semibold font-comforta"
+              onChange={(e) => setSelectedState(e.target.value)}
+            >
+              {states
+                ? states.map((state) => (
+                    <option value={state} selected={state === selectedState}>
+                      {state}
+                    </option>
+                  ))
+                : null}
+            </select>
+          </div>
+          <div className="w-full">
+            <label htmlFor="city">Kabupaten / Kota</label>
+            <select
+              id="city"
+              className="w-full h-10 px-3 py-2 mt-2 focus:ring-1 focus:ring-slate-400 rounded outline-none shadow text-sm font-semibold font-comforta"
+              onChange={(e) => setSelectedCity(e.target.value)}
+            >
+              {cities
+                ? cities.map((city) => (
+                    <option value={city} selected={city === selectedCity}>
+                      {city}
+                    </option>
+                  ))
+                : null}
+            </select>
+          </div>
+        </form>
+        <p className="my-5 text-xl font-semibold font-comforta lg:text-left text-center">
+          {schedule ? schedule.tanggal : null} |{" "}
+          {schedule ? time.toLocaleTimeString() : null}
+        </p>
+        <div className="grid grid-cols-1 lg:grid-cols-4 gap-4 mt-5">
+          <div className="bg-white w-full h-40 p-6 rounded-lg shadow-md flex items-center card-border">
+            <div>
+              <h2 className="text-xl">Imsak</h2>
+              <span className="text-4xl font-semibold">{schedule.imsak}</span>
+            </div>
+          </div>
+          <div className="bg-white w-full h-40 p-6 rounded-lg shadow-md flex items-center card-border">
+            <div>
+              <h2 className="text-xl">Subuh</h2>
+              <span className="text-4xl font-semibold">{schedule.subuh}</span>
+            </div>
+          </div>
+          <div className="bg-white w-full h-40 p-6 rounded-lg shadow-md flex items-center card-border">
+            <div>
+              <h2 className="text-xl">Terbit</h2>
+              <span className="text-4xl font-semibold">{schedule.terbit}</span>
+            </div>
+          </div>
+          <div className="bg-white w-full h-40 p-6 rounded-lg shadow-md flex items-center card-border">
+            <div>
+              <h2 className="text-xl">Dhuha</h2>
+              <span className="text-4xl font-semibold">{schedule.dhuha}</span>
+            </div>
+          </div>
+          <div className="bg-white w-full h-40 p-6 rounded-lg shadow-md flex items-center card-border">
+            <div>
+              <h2 className="text-xl">Dzuhur</h2>
+              <span className="text-4xl font-semibold">{schedule.dzuhur}</span>
+            </div>
+          </div>
+          <div className="bg-white w-full h-40 p-6 rounded-lg shadow-md flex items-center card-border">
+            <div>
+              <h2 className="text-xl">Ashar</h2>
+              <span className="text-4xl font-semibold">{schedule.ashar}</span>
+            </div>
+          </div>
+          <div className="bg-white w-full h-40 p-6 rounded-lg shadow-md flex items-center card-border">
+            <div>
+              <h2 className="text-xl">Maghrib</h2>
+              <span className="text-4xl font-semibold">{schedule.maghrib}</span>
+            </div>
+          </div>
+          <div
+            className={`bg-white w-full h-40 p-6 rounded-lg shadow-md flex items-center card-border`}
           >
-            {states
-              ? states.map((state) => (
-                  <option value={state} selected={state === selectedState}>
-                    {state}
-                  </option>
-                ))
-              : null}
-          </select>
-        </div>
-        <div className="w-full">
-          <label htmlFor="city">Kabupaten / Kota</label>
-          <select
-            id="city"
-            className="w-full h-10 px-3 py-2 mt-2 focus:ring-1 focus:ring-slate-400 rounded outline-none shadow text-sm font-semibold font-comforta"
-            onChange={(e) => setSelectedCity(e.target.value)}
-          >
-            {cities
-              ? cities.map((city) => (
-                  <option value={city} selected={city === selectedCity}>
-                    {city}
-                  </option>
-                ))
-              : null}
-          </select>
-        </div>
-      </form>
-      <p className="my-5 text-xl font-semibold font-comforta lg:text-left text-center">
-        {schedule ? schedule.tanggal : null} |{" "}
-        {schedule ? time.toLocaleTimeString() : null}
-      </p>
-      <div className="grid grid-cols-1 lg:grid-cols-4 gap-4 mt-5">
-        <div className="bg-white w-full h-40 p-6 rounded-lg shadow-md flex items-center card-border">
-          <div>
-            <h2 className="text-xl">Imsak</h2>
-            <span className="text-4xl font-semibold">{schedule.imsak}</span>
-          </div>
-        </div>
-        <div className="bg-white w-full h-40 p-6 rounded-lg shadow-md flex items-center card-border">
-          <div>
-            <h2 className="text-xl">Subuh</h2>
-            <span className="text-4xl font-semibold">{schedule.subuh}</span>
-          </div>
-        </div>
-        <div className="bg-white w-full h-40 p-6 rounded-lg shadow-md flex items-center card-border">
-          <div>
-            <h2 className="text-xl">Terbit</h2>
-            <span className="text-4xl font-semibold">{schedule.terbit}</span>
-          </div>
-        </div>
-        <div className="bg-white w-full h-40 p-6 rounded-lg shadow-md flex items-center card-border">
-          <div>
-            <h2 className="text-xl">Dhuha</h2>
-            <span className="text-4xl font-semibold">{schedule.dhuha}</span>
-          </div>
-        </div>
-        <div className="bg-white w-full h-40 p-6 rounded-lg shadow-md flex items-center card-border">
-          <div>
-            <h2 className="text-xl">Dzuhur</h2>
-            <span className="text-4xl font-semibold">{schedule.dzuhur}</span>
-          </div>
-        </div>
-        <div className="bg-white w-full h-40 p-6 rounded-lg shadow-md flex items-center card-border">
-          <div>
-            <h2 className="text-xl">Ashar</h2>
-            <span className="text-4xl font-semibold">{schedule.ashar}</span>
-          </div>
-        </div>
-        <div className="bg-white w-full h-40 p-6 rounded-lg shadow-md flex items-center card-border">
-          <div>
-            <h2 className="text-xl">Maghrib</h2>
-            <span className="text-4xl font-semibold">{schedule.maghrib}</span>
-          </div>
-        </div>
-        <div className="bg-white w-full h-40 p-6 rounded-lg shadow-md flex items-center card-border">
-          <div>
-            <h2 className="text-xl">Isya</h2>
-            <span className="text-4xl font-semibold">{schedule.isya}</span>
+            <div>
+              <h2 className="text-xl">Isya</h2>
+              <span className="text-4xl font-semibold">{schedule.isya}</span>
+            </div>
           </div>
         </div>
       </div>
-
-      <footer className="text-center mt-16">
+      <footer className="text-center mt-10 py-4 border-t border-slate-200">
         <p>
           Made with ❤️ by{" "}
           <a
@@ -186,6 +189,6 @@ export default function App() {
           </a>
         </p>
       </footer>
-    </div>
+    </>
   );
 }
